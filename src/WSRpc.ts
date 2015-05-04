e5d19b3492ae73e6bbc01c054ca23a8331947736
Lionel G <lionelg3@gmail.com>
2015-05-04 21:13:01 +0200

    Create KeyRegistry

--- src/WSRpc.ts
+++ src/WSRpc.ts
@@ -7,6 +7,7 @@ export module lg3x {
         export interface IRPCRegistry {
             register(name: string, classname: any);
             invoke(name: string, method: string, params?: any): any;
+			get(name: string): any;
         }
 
         export interface IWSRpc {
@@ -206,7 +207,11 @@ export module lg3x {
                 throw (msg);
             }
         }
-    }
+
+		get(name: string): any {
+			return this._objects[name];
+		}
+	}
 
     export class ClassRegistry implements api.IRPCRegistry {
         private _classNames: { [name: string]: any } = {};
@@ -226,9 +231,52 @@ export module lg3x {
                 throw (msg);
             }
         }
+
+		get(name: string): any {
+			return this._classNames[name];
+		}
     }
 
-    export class RPC {
+	export class KeyRegistry implements api.IRPCRegistry {
+		private _key: string;
+		private _classRegistry: ClassRegistry;
+		private _objects: { [name: string]: Object } = {};
+		constructor(key: string, registry: ClassRegistry) {
+			this._key = key;
+			this._classRegistry = registry;
+		}
+
+		register(name: string, className: any) {
+			throw 'Register method should not be use on KeyRegistry';
+		}
+
+		invoke(name: string, method: string, params?: any): any {
+			try {
+				var instance = this._objects[this._key + ':' + name];
+				if (!instance) {
+					console.log('create instance');
+					var className = this._classRegistry.get(name);
+					this._objects[this._key + ':' + name] = new className();
+					instance = this._objects[this._key + ':' + name];
+				}
+				return instance[method](params);
+			} catch (err) {
+				var msg: string = 'KeyRegistry ' + this._key + ' invoke fail on ' + name + '.' + method;
+				msg += (params) ? '(' + JSON.stringify(params) + ')' : '()';
+				throw (msg);
+			}
+		}
+
+		key() {
+			return this._key;
+		}
+
+		get(name: string): any {
+			return this._classRegistry.get(name);
+		}
+	}
+
+	export class RPC {
 
         public static VERSION: string = '2.0';
         public static PARSE_ERROR: number = -32700;
